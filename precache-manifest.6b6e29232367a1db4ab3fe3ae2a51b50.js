65087d0b5a50abc1a9d536881baec69f8e4b752a
Zeinab Farag <zeinabfarag@Zeinabs-MacBook-Pro.local>
2020-04-22 15:12:06 -0400

    Updates

---
+++ precache-manifest.6b6e29232367a1db4ab3fe3ae2a51b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3421a95b00ab472782d6005e0122f376",
+    "url": "/2020-04-22-Noisemaker/index.html"
+  },
+  {
+    "revision": "3f10d2b9c9a9677605fa",
+    "url": "/2020-04-22-Noisemaker/static/css/main.2db8c41b.chunk.css"
+  },
+  {
+    "revision": "04ad478fd0f798550f77",
+    "url": "/2020-04-22-Noisemaker/static/js/2.9ed1ecec.chunk.js"
+  },
+  {
+    "revision": "b28590a4d5a036be9947256ff9f1f35b",
+    "url": "/2020-04-22-Noisemaker/static/js/2.9ed1ecec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f10d2b9c9a9677605fa",
+    "url": "/2020-04-22-Noisemaker/static/js/main.991d1646.chunk.js"
+  },
+  {
+    "revision": "e9c263f69e667b40def4",
+    "url": "/2020-04-22-Noisemaker/static/js/runtime-main.c7ae0f90.js"
+  }
+]);
